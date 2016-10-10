59e965047e66451cdc3375bc6e194335daa13b8b
Joshua Seigler <joshua.seigler@xpo.com>
2016-10-10 12:47:11 -0400

    feat: syncronize position across several windows

--- scripts/app.js
+++ scripts/app.js
@@ -9,15 +9,24 @@
   function $(selector) {
     return document.querySelector(selector);
   }
-  function $$(selector){
+  function $$(selector) {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
   }
 
-  function moveSlides() {
+  function setCurrentSlide(newSlideNumber) {
+    newSlideNumber = Math.max(Math.min(newSlideNumber, slides.length - 1), 0);
+    if (newSlideNumber !== currentSlideNumber) {
+      currentSlideNumber = newSlideNumber;
+      localStorage.setItem('janus-currentSlideNumber', currentSlideNumber);
+    }
     slides.forEach(function (item, index, array) {
       if (index < currentSlideNumber) {
-        item.setAttribute('janus-timeline', 'past');
-      } else if (index == currentSlideNumber) {
+        if (slides[index].contains(slides[currentSlideNumber])) {
+          item.setAttribute('janus-timeline', 'present');
+        } else {
+          item.setAttribute('janus-timeline', 'past');
+        }
+      } else if (index === currentSlideNumber) {
         item.setAttribute('janus-timeline', 'present');
       } else {
         item.setAttribute('janus-timeline', 'future');
@@ -25,26 +34,34 @@
     });
   }
 
+  var sessionListener = function(e) {
+    if (e.url === window.location.href) {
+      if (e.key === 'janus-currentSlideNumber') {
+        setCurrentSlide(+e.newValue);
+      }
+    }
+  };
+
   var init = function() {
-    slides = $$('main>section');
+    slides = $$('main>section, [janus-timeline]');
     currentSlideNumber = 0;
     shortcut.add('F1', function() {
       document.body.classList.toggle('show-notes');
     });
     shortcut.add('Page_down', function() {
-      if (currentSlideNumber < slides.length - 1) {
-        currentSlideNumber++;
-        moveSlides();
-      }
+      setCurrentSlide(currentSlideNumber + 1);
     });
     shortcut.add('Page_up', function() {
-      if (currentSlideNumber > 0) {
-        currentSlideNumber--;
-        moveSlides();
-      }
+      setCurrentSlide(currentSlideNumber - 1);
     });
-    moveSlides();
-  }
+    var storedSlideNumber;
+    if (storedSlideNumber = localStorage.getItem('janus-currentSlideNumber')) {
+      setCurrentSlide(storedSlideNumber);
+    } else {
+      setCurrentSlide(0);
+    }
+  };
 
-  document.addEventListener("DOMContentLoaded", init);
+  document.addEventListener('DOMContentLoaded', init);
+  window.addEventListener('storage', sessionListener);
 })();
